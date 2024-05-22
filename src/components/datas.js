34caac961356047835a785892cde4c0b8a31ccf8
arsene <fopoar@gmail.com>
2024-05-22 09:09:42 -0300

    table de codeurs

---
+++ src/components/datas.js
@@ -0,0 +1,20 @@
+
+const codeurs = [
+	{
+		id: "1",
+		name: "Shivansh",
+        experience:"JUNIOR"
+	},
+	{
+		id: "2",
+		name: "Simran",
+        experience:"SEINIOR"
+	},
+	{
+		id: "3",
+		Name: "Aakash",
+        experience:"JUNIOR"
+	},
+];
+
+export default array;
